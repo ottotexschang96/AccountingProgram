cfb9c090e870a22432bc4e492dc7e20aa4eaa36f
Otto Chang <schang96@gatech.edu>
2021-03-30 09:47:09 -0700

    2nd Ver.

--- AccountingProgram.py
+++ AccountingProgram.py
@@ -17,3 +17,13 @@ print(product)
 for p in product:
 	print(p)
 	print(p[0], 's price is', p[1])
+
+# +* can be applied on string, but not -/
+# 'abc' + '123' = 'abc123'
+# 'abc' * 3 = 'abcabcabc'
+
+# can be reviewed in Chapter4\20210327\Read
+with open('AccountingProgram.csv', 'w') as f: # use more common .csv, which can be opened by excel, to replace .txt
+	for p in product:
+		f.write(p[0] + ',' + p[1] + '\n') 
+#f's function 'write' should only include one argument/srting like append() takes exactly one argument (1 given)
